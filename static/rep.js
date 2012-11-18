24479af8e438b270a71a36f20fce86c65fd77735
Sidney San Martín <s@sidneysm.com>
2012-11-18 09:04:34 -0500

    Add rep.js!

---
+++ static/rep.js
@@ -0,0 +1,35 @@
+var newCallTemplate = document.getElementById("newCallTemplate").innerText;
+$("#customerList")
+	.append(Mustache.render(newCallTemplate, {
+		title: "Jane Doe",
+		subtitle: "(212) 555-1234",
+		status: "finished",
+		statusText: "Finished"
+	}))
+	.append(Mustache.render(newCallTemplate, {
+		title: "Bill Smith",
+		subtitle: "(212) 555-1234",
+		status: "finished",
+		statusText: "Finished"
+	}))
+;
+
+var socket = io.connect('/agent');
+socket.on("call", function(data){
+	var templateData = {
+		status: "waiting",
+		statusText: "Waiting"
+	};
+	var $newCall;
+	if ((templateData.title = data.name || data.email)) {
+		templateData.subtitle = data.phoneNum;
+	} else {
+		templateData.title = data.phoneNum;
+		templateData.subtitle = "New Jersey"; // lol
+	}
+	$newCall = $(Mustache.render(newCallTemplate, templateData));
+	$newCall.one('click', function(){
+		$(this).addClass('active');
+	});
+	$("#customerList").prepend($newCall);
+});
